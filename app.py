e0969c1e0684b15a4de7732eb3cf1354caf5a432
xinonbissness-cloud <xinonbissness@gmail.com>
2026-09-20 10:00:26 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
 from flask import Flask
+from config import Config
+from models.user import db
 
 app = Flask(__name__)
+app.config.from_object(Config)
+
+db.init_app(app)
 
 
 @app.route("/")
 def home():
-    return "Xinon Social Media is running!"
+    return "Xinon Social Backend is running!"
+
+
+with app.app_context():
+    db.create_all()
 
 
 if __name__ == "__main__":
