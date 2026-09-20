f91fd0ca47671114d2db630394d72f71b12a2ad8
xinonbissness-cloud <xinonbissness@gmail.com>
2026-09-20 10:39:07 +0700

    Update auth.py

--- routes/auth.py
+++ routes/auth.py
@@ -1,24 +1,44 @@
-from flask import Blueprint, request, jsonify
-from models.user import db, User
 from datetime import datetime
+
+from flask import Blueprint, request, jsonify
+
+from models.user import db, User
+
 
 auth_bp = Blueprint("auth", __name__)
 
 
 @auth_bp.route("/register", methods=["POST"])
 def register():
-    data = request.get_json()
+    data = request.get_json(silent=True) or {}
 
     name = data.get("name")
-    birthday = data.get("birthday")
+    birthday_text = data.get("birthday")
     gender = data.get("gender")
     username = data.get("username")
     email = data.get("email")
     password = data.get("password")
 
-    if not all([name, birthday, gender, username, email, password]):
+    if not all([
+        name,
+        birthday_text,
+        gender,
+        username,
+        email,
+        password
+    ]):
         return jsonify({
             "error": "All fields are required"
+        }), 400
+
+    try:
+        birthday = datetime.strptime(
+            birthday_text,
+            "%Y-%m-%d"
+        ).date()
+    except ValueError:
+        return jsonify({
+            "error": "Birthday must use YYYY-MM-DD format"
         }), 400
 
     if User.query.filter_by(email=email).first():
@@ -52,7 +72,7 @@ def register():
 
 @auth_bp.route("/login", methods=["POST"])
 def login():
-    data = request.get_json()
+    data = request.get_json(silent=True) or {}
 
     email = data.get("email")
     password = data.get("password")
