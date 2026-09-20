752ae5454d37189fc763211b86433a9cfc911826
xinonbissness-cloud <xinonbissness@gmail.com>
2026-09-20 10:24:41 +0700

    Update user.py

--- models/user.py
+++ models/user.py
@@ -8,9 +8,24 @@ class User(db.Model):
     __tablename__ = "users"
 
     id = db.Column(db.Integer, primary_key=True)
+
+    name = db.Column(db.String(100), nullable=False)
+
+    birthday = db.Column(db.Date, nullable=False)
+
+    gender = db.Column(db.String(20), nullable=False)
+
     username = db.Column(db.String(50), unique=True, nullable=False)
+
     email = db.Column(db.String(120), unique=True, nullable=False)
+
     password_hash = db.Column(db.String(255), nullable=False)
+
+    email_verified = db.Column(db.Boolean, default=False, nullable=False)
+
+    verification_code = db.Column(db.String(10), nullable=True)
+
+    verification_expires = db.Column(db.DateTime, nullable=True)
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
