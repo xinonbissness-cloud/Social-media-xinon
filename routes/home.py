9792d369605e42f822d1338b16693de3c3a64fa8
xinonbissness-cloud <xinonbissness@gmail.com>
2026-09-22 08:09:15 +0700

    Delete routes directory

--- routes/home.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-home_bp = Blueprint("home", __name__)
-
-@home_bp.route("/home")
-def page():
-    return render_template("home.html")
--- routes/profile.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-profile_bp = Blueprint("profile", __name__)
-
-@profile_bp.route("/profile")
-def page():
-    return render_template("profile.html")
