e2f2156ddbeaf2b9ce9ed1e59af3ed679854dfb8
xinonbissness-cloud <xinonbissness@gmail.com>
2026-09-22 08:54:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ from flask_cors import CORS
 from config import Config
 from models.user import db
 from routes.auth import auth_bp
+from routes.posts import posts_bp
+from models.post import Post
 
 app = Flask(__name__)
 app.config.from_object(Config)
 CORS(app)
 db.init_app(app)
 app.register_blueprint(auth_bp, url_prefix="/api/auth")
+app.register_blueprint(posts_bp, url_prefix="/api")
 
 @app.route("/")
 def root():
